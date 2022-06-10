f7ef889e16a4f81a49e34d748602c706a454002c
Pololops <paul.rigaudeau@me.com>
2022-06-10 11:11:53 +0200

    create react app and structure

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import WeatherWidget from '../WeatherWidget';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <WeatherWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherWidget/index.js
@@ -0,0 +1,18 @@
+// import PropTypes from 'prop-types';
+import './style.scss';
+
+export default function WeatherWidget({ props }) {
+    return (
+        <div className="widget">
+         Weather Widget
+        </div>
+    );
+}
+
+// WeatherWidget.propTypes = {
+// props: PropTypes.number|string|bool|func|array|object|node.isRequired,
+// };
+
+// WeatherWidget.defaultProps = {
+// props: '',
+// };
