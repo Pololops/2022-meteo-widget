b75aeea0fe8aaca1efb22c4756bc1a7182b3e939
Pololops <paul.rigaudeau@me.com>
2022-06-10 14:08:32 +0200

    JSX + styles

--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -3,10 +3,15 @@ import './style.scss';
 
 export default function WeatherWidget({ props }) {
     return (
-        <div className="widget">
-         Weather Widget
-        </div>
-    );
+		<div className="widget">
+			<div className="widget__infos">
+				<p className="widget__city">Paris</p>
+				<p className="widget__desc">Poussièreux</p>
+			</div>
+			<p className="widget__temperature">15<sup>°C</sup></p>
+            <input type="text" placeholder="Ville" />
+		</div>
+	);
 }
 
 // WeatherWidget.propTypes = {
